f945b540bf9e5a33c3d65a65de7f446fddaa79de
JMIHC <John@bahai.us>
2015-06-22 17:07:01 -0500

    Fixing an issue in the settings.py to reflect correct static url

--- coffeed/settings.py
+++ coffeed/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/Static/'
 
 STATICFILES_DIRS = (
     os.path.join(MAIN_DIR, 'static'),
